f2b95317862386e4a084fadf85f5460bf7e3d1e7
David Liu <david-khala@hotmail.com>
2024-11-01 11:24:56 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 # my standard config
-git config pull.rebase true
+git config --global pull.rebase true
 
 git config --global user.name "David Liu"
 git config --global user.email david-khala@hotmail.com
